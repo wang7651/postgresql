e9dd96fc5244fb346d54911433d24a290804a828
徐國堂 <roberthsu2003@gmail.com>
2025-07-13 03:33:43 +0000

    modify

---
+++ lesson5/lesson5_2.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS employee;
+
+CREATE TABLE employee(
+	emp_id SERIAL,
+	name VARCHAR(20),
+	birth_date DATE,
+	sex VARCHAR(1),
+	salary INT,
+	branch_id INT,
+	sup_id INT,
+ 	PRIMARY KEY(emp_id)
+);
