4f25d0d80dbd10b51608120c08e83b4a3aac168d
徐國堂 <roberthsu2003@gmail.com>
2025-07-13 04:03:49 +0000

    modify

--- lesson5/lesson5_2.sql
+++ lesson5/lesson5_2.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS employee;
+DROP TABLE IF EXISTS  branch;
 
 CREATE TABLE employee(
 	emp_id SERIAL,
@@ -10,3 +11,20 @@ CREATE TABLE employee(
 	sup_id INT,
  	PRIMARY KEY(emp_id)
 );
+
+CREATE TABLE branch(
+	branch_id INT,
+	branch_name VARCHAR(20),
+	manager_id INT,
+	PRIMARY KEY(branch_id),
+	FOREIGN KEY(manager_id)
+	REFERENCES employee(emp_id) ON DELETE SET NULL
+);
+
+ALTER TABLE employee ADD FOREIGN KEY(branch_id)
+REFERENCES branch(branch_id) ON DELETE SET NULL;
+
+ALTER TABLE employee ADD FOREIGN KEY(sup_id)
+REFERENCES employee(emp_id) ON DELETE SET NULL;
+
+
